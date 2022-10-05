f664b9237bfaeeb4b6f65751f4c2b4624bb40c14
naegonggae <https://github.com/naegonggae/lion>
2022-10-05 10:23:17 +0900

    CalculatorTest 입니다.

---
+++ day2/Calculator.java
@@ -0,0 +1,25 @@
+package day2;
+
+public class Calculator {
+    protected int a;
+    protected int b;
+
+    public Calculator(int a, int b) {
+        this.a = a;
+        this.b = b;
+
+    }
+
+    public int plus() {
+        return a+b;
+    }
+    public int minus() {
+        return a-b;
+    }
+    public int multiple() {
+        return a*b;
+    }
+    public int divide() {
+        return a/b;
+    }
+}
---
+++ day2/CalculatorTest.java
@@ -0,0 +1,11 @@
+package day2;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        Calculator c = new Calculator(1,2);
+        System.out.println(c.plus());
+
+
+
+    }
+}
